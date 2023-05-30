178fdac7430e2d504df3f494f2268d9188cec82e
chanho6e1 <gn05211@gmail.com>
2023-05-30 09:48:10 +0900

    [Gold IV] Title: Puyo Puyo, Time: 120 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Gold/11559. Puyo Puyo/Puyo Puyo.java
+++ 백준/Gold/11559. Puyo Puyo/Puyo Puyo.java
@@ -1,19 +1,21 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.StringTokenizer;
 
 public class Main {
 
-    static char[][] map;
-    static boolean isPop = false;
-
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
+
+    static boolean check;
+    static int result;
+    static char[][] map;
+    static boolean[][] visited;
+    static ArrayList<int[]> list;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -23,16 +25,13 @@ public class Main {
             map[i] = br.readLine().toCharArray();
         }
 
-        int result = 0;
-
         while (true) {
-            isPop = false;
+            check = false;
 
-            bfs(); // 연쇄가 가능한 뿌요들 터트리기
-            onFloor(); // 뿌요들을 맨 아래로 내리기
+            bfs();
+            down();
 
-            if (!isPop)
-                break;
+            if (!check) break;
 
             result++;
         }
@@ -40,79 +39,72 @@ public class Main {
         System.out.println(result);
     }
 
+    private static void down() {
+        for (int j = 0; j < 6; j++) {
+            Queue<Character> q = new LinkedList<>();
+            int idx = 11;
+
+            for (int i = idx; i >= 0; i--) {
+                if (map[i][j] == '.') continue;
+
+                q.offer(map[i][j]);
+
+                map[i][j] = '.';
+            }
+
+            while (!q.isEmpty()) {
+                map[idx--][j] = q.poll();
+            }
+        }
+    }
+
     private static void bfs() {
-        Queue<Puyo> q = new LinkedList<>();
-        boolean visited[][] = new boolean[12][6];
+        visited = new boolean[12][6];
+        Queue<int[]> q = new LinkedList<>();
 
         for (int i = 0; i < 12; i++) {
             for (int j = 0; j < 6; j++) {
                 if (map[i][j] == '.' || visited[i][j]) continue;
 
-                ArrayList<int[]> list = new ArrayList<>();
-                q.offer(new Puyo(i, j, map[i][j]));
-                list.add(new int[]{i, j});
+                q.offer(new int[]{i, j});
                 visited[i][j] = true;
 
+                list = new ArrayList<>();
+                list.add(new int[]{i, j});
+
                 while (!q.isEmpty()) {
-                    Puyo now = q.poll();
+                    int[] now = q.poll();
 
                     for (int d = 0; d < 4; d++) {
-                        int nx = now.x + dx[d];
-                        int ny = now.y + dy[d];
+                        int nx = now[0] + dx[d];
+                        int ny = now[1] + dy[d];
 
                         if (nx < 0 || ny < 0 || nx >= 12 || ny >= 6) continue;
                         if (visited[nx][ny]) continue;
-                        if (map[nx][ny] != now.color) continue;
+                        if (map[nx][ny] != map[now[0]][now[1]]) continue;
 
-                        q.offer(new Puyo(nx, ny, map[nx][ny]));
+                        q.offer(new int[]{nx, ny});
                         list.add(new int[]{nx, ny});
                         visited[nx][ny] = true;
                     }
                 }
 
-                if (list.size() >= 4){
-                    for (int k=0; k<list.size(); k++){
-                        int x = list.get(k)[0];
-                        int y = list.get(k)[1];
+                if (list.size() >= 4) {
+                    check = true;
 
-                        map[x][y] = '.';
-
-                        isPop = true;
+                    for (int k = 0; k < list.size(); k++) {
+                        map[list.get(k)[0]][list.get(k)[1]] = '.';
                     }
                 }
             }
         }
     }
 
-    private static void onFloor() {
-        for (int j=0; j<6; j++)
-            down(j);
-    }
-
-    private static void down(int j) {
-        Queue<Puyo> q = new LinkedList<>();
-        int idx = 11;
-
-        for (int i = 11; i>=0; i--){
-            if (map[i][j] != '.'){
-                q.offer(new Puyo(i, j, map[i][j]));
-                map[i][j] = '.';
-            }
-        }
-
-        while (!q.isEmpty()){
-            Puyo now = q.poll();
-
-            map[idx][j] = now.color;
-            idx--;
-        }
-    }
-
-    static class Puyo {
+    static class Data {
         int x, y;
         char color;
 
-        Puyo(int x, int y, char color) {
+        public Data(int x, int y, char color) {
             this.x = x;
             this.y = y;
             this.color = color;
