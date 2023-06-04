b73e3c415f32025c250d49c7ad7029f620d13deb
chanho6e1 <gn05211@gmail.com>
2023-06-04 18:35:39 +0900

    [level 1] Title: 소수 찾기, Time: 177.42 ms, Memory: 51.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    
+    public int solution(int n) {
+        int answer = 0;
+        
+        for(int i=2; i<=n; i++){
+            if (isPrime(i)) answer++;
+        }
+        
+        return answer;
+    }
+    
+    private static boolean isPrime(int num){
+        
+        for(int i=2; i<=Math.sqrt(num); i++){
+            if(num % i == 0) return false;
+        }
+        
+        return true;
+    }
+}
